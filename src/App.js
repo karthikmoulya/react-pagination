bc506a41b478bd85e92c87b400f2609177e28dda
karthikmoulya <karthikmoulya1995@gmail.com>
2024-08-20 20:18:52 +0530

    practiced

--- src/App.js
+++ src/App.js
@@ -3,69 +3,63 @@ import { useEffect, useState } from 'react';
 const App = () => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
 
   const fetchProducts = async () => {
-    const res = await fetch(
-      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
-    );
+    const res = await fetch('https://dummyjson.com/products');
     const data = await res.json();
 
     if (data && data.products) {
       setProducts(data.products);
-      setTotalPages(data.total);
-    }
-  };
-
-  const selectPageHandler = (selectedPage) => {
-    if (
-      selectedPage >= 1 &&
-      selectedPage <= totalPages &&
-      page !== selectedPage
-    ) {
-      setPage(selectedPage);
     }
   };
 
   useEffect(() => {
     fetchProducts();
-  }, [page]);
+  }, []);
+
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
+    }
+  };
 
   return (
     <div>
       {products.length > 0 && (
-        <div className='products'>
-          {products.map((product) => {
+        <span className='products'>
+          {products.slice(page * 10 - 10, page * 10).map((product) => {
             return (
-              <span key={product.id} className='products__single'>
+              <span className='products__single' key={product.id}>
                 <img src={product.thumbnail} alt={product.title} />
                 <span>{product.title}</span>
               </span>
             );
           })}
-        </div>
+        </span>
       )}
 
       {products.length > 0 && (
-        <div className='pagination'>
+        <span className='pagination'>
           {page > 1 && (
-            <span onClick={() => selectPageHandler(page - 1)}>◀️</span>
-          )}
-          {[...Array(totalPages / 10)].map((_, i) => {
-            return (
-              <span
-                key={i}
-                className={page === i + 1 ? 'pagination__selected' : ''}
-                onClick={() => selectPageHandler(i + 1)}
-              >
-                {i + 1}
-              </span>
-            );
-          })}
-          {page < totalPages / 10 && (
             <span onClick={() => selectPageHandler(page + 1)}>▶️</span>
           )}
-        </div>
+          {[...Array(products.length / 10)].map((_, i) => (
+            <span
+              onClick={() => selectPageHandler(i + 1)}
+              key={i}
+              className={page === i + 1 ? 'pagination__selected' : ''}
+            >
+              {i + 1}
+            </span>
+          ))}
+          {page < products.length / 10 && (
+            <span onClick={() => selectPageHandler(page - 1)}>◀️</span>
+          )}
+        </span>
       )}
     </div>
   );
